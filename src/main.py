8bc39e2df28db83422963f8d67b123ec9f81dbc6
Carlos Daniel <ccdd.dev@gmail.com>
2022-01-23 13:35:52 -0600

    feat: emit/subscribe topics

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+from time import sleep
 from config import config
 from utils import get_logger
+from hedera import TopicId
 from utils.hedera import Hedera, HederaAccount
+from utils.topic import TopicMessage, Topic, TopicSubscription
 
 logger = get_logger(__name__)
 
@@ -15,16 +18,18 @@ root_account: HederaAccount = HederaAccount(
     client, account_id=root_account_id, private_key=root_private_key
 )
 logger.info(f"Root account: {root_account}")
+logger.info("\n\n")
 
 # Create a new account
 new_account: HederaAccount = HederaAccount(client)
 logger.info(f"New account: {new_account}")
-
-
 logger.info("\n\n")
-logger.info("Tranfer from root account to new account")
+
+
+logger.info("Tranfer HBAR from root account to new account")
+tinybars_transfer_amount = 100_000
 txr = root_account.transfer(
-    tinybars=100_000, account_id=new_account.account_id, memo="welcome"
+    tinybars=tinybars_transfer_amount, account_id=new_account.account_id, memo="welcome"
 )
 logger.info(f"Transfer status: {root_account.tx_status(txr).toString()}")
 logger.info(f"Transfer cost: {root_account.tx_receipt(txr).toString()}")
@@ -32,3 +37,29 @@ logger.info(f"Transfer cost: {root_account.tx_receipt(txr).toString()}")
 logger.info("\n\n")
 logger.info(f"root account balance: {root_account.get_balance().hbars.toString()}")
 logger.info(f"new account balance: {new_account.get_balance().hbars.toString()}")
+logger.info("\n\n")
+
+topic_name = "Token Transfer"
+logger.info(f"Create new topic: {topic_name}")
+topic: Topic = Topic(client=client, memo=topic_name)
+topic_id: TopicId = topic.get_id()
+logger.info(f"Topic ID: {topic_id.toString()}")
+logger.info("\t>Waiting for topic creation (consensus)...")
+sleep(5)
+logger.info("\n\n")
+
+logger.info(f"Subscribe to topic")
+sub1: TopicSubscription = TopicSubscription(client=client, topic_txr=topic.txr)
+sub2: TopicSubscription = TopicSubscription(client=client, topic_txr=topic.txr)
+
+
+logger.info(f"Publish message to topic: {topic_name}")
+publisher: TopicMessage = TopicMessage(client=client, topic_id=topic_id)
+
+publisher.emit(
+    f"Account {new_account.account_id.toString()} received {tinybars_transfer_amount} tinybars"
+)
+
+logger.info("Waiting for subscribers to react to emitted message...")
+sleep(10)
+logger.info("Waiting time finished")
---
+++ src/utils/topic.py
@@ -0,0 +1,63 @@
+from typing import Optional
+
+from hedera import (
+    Client,
+    TopicId,
+    PyConsumer,
+    TopicCreateTransaction,
+    TopicMessageQuery,
+    TopicMessageSubmitTransaction,
+    TransactionResponse,
+    TopicId,
+)
+
+from utils import get_logger
+
+logger = get_logger(__name__)
+
+
+class Topic:
+    def __init__(self, client: Client, memo: Optional[str] = "N/A") -> None:
+        self.client = client
+        self.topic: TopicCreateTransaction = TopicCreateTransaction().setTopicMemo(memo)
+        self.txr: TransactionResponse = self.topic.execute(self.client)
+
+    def get_id(self) -> TopicId:
+        return self.txr.getReceipt(self.client).topicId
+
+
+class TopicSubscription:
+    def __init__(self, client: Client, topic_txr: TransactionResponse) -> None:
+        self.client = client
+        self.topic_txr = topic_txr
+        self.topic_id = self.topic_txr.getReceipt(self.client).topicId
+
+        self.subscription: TopicMessageQuery = (
+            TopicMessageQuery()
+            .setTopicId(self.topic_id)
+            .subscribe(
+                self.client,
+                PyConsumer(
+                    lambda timestamp, argc, *argv: logger.info(
+                        f"subscription result:\n\tTimestamp: {timestamp}\n\tPayload size: {argc}\n\tPayload: {argv}"
+                    )
+                ),
+            )
+        )
+
+
+class TopicMessage:
+    def __init__(self, client: Client, topic_id: TopicId) -> None:
+        self.client = client
+        self.topic_id = topic_id
+
+    def emit(self, msg: str) -> TransactionResponse:
+
+        txr: TransactionResponse = (
+            TopicMessageSubmitTransaction()
+            .setTopicId(self.topic_id)
+            .setMessage(msg)
+            .execute(self.client)
+        )
+
+        return txr
