67e03167ce0a170081869ee4ffbaa4d30fb8e5e6
Carlos Daniel <ccdd.dev@gmail.com>
2022-01-22 20:20:04 -0600

    ref: Hedera helper class for get_client

--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
-from utils.hedera import get_client
-
-
-from config import DeploymentEnv, config
+from config import config
 from utils import get_logger
+from utils.hedera import Hedera
 
 logger = get_logger(__name__)
 
 logger.info(f"Config: {config}")
 
-client = get_client()
-logger.info(f"hedera client: {client}")
+client = Hedera.get_client()
--- src/utils/hedera.py
+++ src/utils/hedera.py
@@ -6,21 +6,26 @@ from utils import get_logger
 logger = get_logger(__name__)
 
 
-def get_client():
-    logger.debug(f"get_client - create account_id")
-    account_id = AccountId.fromString(config["account"]["id"])
+class Hedera:
+    @staticmethod
+    def get_client() -> Client:
+        logger.debug(f"Hedera::get_client - create account_id")
+        account_id: AccountId = AccountId.fromString(config["account"]["id"])
 
-    logger.debug(f"get_client - create account_id")
-    private_key = PrivateKey.fromString(config["account"]["private_key"])
+        logger.debug(f"Hedera::get_client - create account_id")
+        private_key: PrivateKey = PrivateKey.fromString(
+            config["account"]["private_key"]
+        )
 
-    logger.debug(f"get_client - create client")
-    client = (
+        logger.debug(f"Hedera::get_client - create client")
+        client: Client = (
         Client.forTestnet()
         if config["env"] == DeploymentEnv.Development.value
         else Client.forMainnet()
     )
 
-    logger.debug(f"get_client - set operator")
+        logger.debug(f"Hedera::get_client - set operator")
     client.setOperator(account_id, private_key)
 
+        logger.debug(f"Hedera::get_client - client: {client.toString()}")
     return client
