6d51375442cb158b0c28c3188f61be05d20beb83
Carlos Daniel <ccdd.dev@gmail.com>
2022-01-22 20:20:28 -0600

    feat: create new Hedera account

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from config import config
 from utils import get_logger
-from utils.hedera import Hedera
+from utils.hedera import Hedera, HederaAccount
 
 logger = get_logger(__name__)
 
 logger.info(f"Config: {config}")
 
 client = Hedera.get_client()
+
+account: HederaAccount = HederaAccount(client)
+account.get_balance()
--- src/utils/hedera.py
+++ src/utils/hedera.py
@@ -1,4 +1,15 @@
-from hedera import AccountId, Client, PrivateKey
+from hedera import (
+    Hbar,
+    AccountCreateTransaction,
+    AccountId,
+    Client,
+    PrivateKey,
+    PublicKey,
+    AccountBalance,
+    AccountBalanceQuery,
+    TransactionResponse,
+    TransactionReceipt,
+)
 
 from config import DeploymentEnv, config
 from utils import get_logger
@@ -19,13 +30,41 @@ class Hedera:
 
         logger.debug(f"Hedera::get_client - create client")
         client: Client = (
-        Client.forTestnet()
-        if config["env"] == DeploymentEnv.Development.value
-        else Client.forMainnet()
-    )
+            Client.forTestnet()
+            if config["env"] == DeploymentEnv.Development.value
+            else Client.forMainnet()
+        )
 
         logger.debug(f"Hedera::get_client - set operator")
-    client.setOperator(account_id, private_key)
+        client.setOperator(account_id, private_key)
 
         logger.debug(f"Hedera::get_client - client: {client.toString()}")
-    return client
+        return client
+
+
+class HederaAccount:
+    def __init__(self, client) -> None:
+        self.private_key: PrivateKey = PrivateKey.generate()
+        self.public_key: PublicKey = self.private_key.getPublicKey()
+        self.client: Client = client
+
+        tx_resp: TransactionResponse = (
+            AccountCreateTransaction()
+            .setKey(self.public_key)
+            .setInitialBalance(Hbar.fromTinybars(1000))
+            .execute(client)
+        )
+
+        self.tx_receipt: TransactionReceipt = tx_resp.getReceipt(self.client)
+        self.account_id: AccountId = self.tx_receipt.accountId
+        logger.debug(
+            f"HederaAccount::init - new account id: {self.account_id.toString()}"
+        )
+
+    def get_balance(self):
+        balance: AccountBalance = (
+            AccountBalanceQuery().setAccountId(self.account_id).execute(self.client)
+        )
+        logger.debug(
+            f"HederaAccount::get_balance - balance: {balance.hbars.toString()}"
+        )
